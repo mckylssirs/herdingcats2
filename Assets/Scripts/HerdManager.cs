1fe44b98af871c43a8d907b4865ebe4275f602e5
Peter Macpherson <p.a.macpherson@gmail.com>
2020-12-29 08:46:56 -0700

    Updated movement

--- Assets/Scripts/HerdManager.cs
+++ Assets/Scripts/HerdManager.cs
@@ -10,26 +10,46 @@ public class HerdManager : MonoBehaviour
     public int nCats = 10;
     public float speed = 5.0f;
     private List<GameObject> oldHerd = new List<GameObject>();
-   
+
     // Start is called before the first frame update
     void Start()
     {
-        float rotateX,rotateY;
+        float rotateX, rotateY;
         for (int i = 0; i < nCats; i++) {
-            rotateX = Random.Range(-1.0f, 1.0f);
+            rotateX = Random.Range(0, 360f);
             rotateY = Random.Range(-1.0f, 1.0f);
-            GameObject newCat = Instantiate(cat, new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-10.0f, 10.0f), 0),
+            GameObject newCat = Instantiate(cat, new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-5.0f, 5.0f), 0),
                 Quaternion.identity);
-     //       newCat.transform.rotation = new Vector3(rotateX, rotateY, 0);
+    //        newCat.transform.localRotation = Quaternion.Euler(new Vector3(0, 0, rotateX));
+            //       newCat.transform.rotation = new Vector3(rotateX, rotateY, 0);
             Rigidbody2D rb = newCat.GetComponent<Rigidbody2D>();
+            rotateX = rotateX * 3.1415962535f / 180.0f;
             
+            rb.velocity = new Vector2(Mathf.Cos(rotateX * speed), Mathf.Sin(rotateX * speed));
+            newCat.transform.eulerAngles = Vector3.forward * rotateX;
+//            newCat.transform.right = rb.velocity.normalized;
             oldHerd.Add(newCat);
         }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        Vector2 pos;
+        foreach (GameObject cat in oldHerd)
+        {
+            Rigidbody2D rb = cat.GetComponent<Rigidbody2D>();
+            pos = rb.position;
+            if (pos.x>10 || pos.x<-10) {
+               rb.velocity=new Vector2(-rb.velocity.x, rb.velocity.y);
+                rb.position = pos + rb.velocity;
+            }
+            if (pos.y > 5 || pos.y < -5)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -rb.velocity.y);
+                rb.position = pos + rb.velocity;
+            }
+            cat.transform.right = rb.velocity.normalized;
+        }
+
     }
 }
